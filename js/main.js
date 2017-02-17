754f11c0fc923c5dad05b35cc541fb96a3bc8f43
Clinton <c_clinton2@tss-hr420-dm39.fc.ca>
2017-02-17 16:09:59 -0500

    geolocation fully working

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 (function() {
 
-var map, marker;
+var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker;
 
-function initMap(){
+function initMap(position){
 
-	map = new google.maps.Map(document.querySelector('.map-wrapper'), {
+	map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude }); //passes dynamic variables through method NOTE: objects require commas, methods require semicolons
+	map.setZoom(14);
 
-		center: {lat: 42.983233, lng: -81.250688},
-		zoom : 14
+	marker = new google.maps.Marker({
+	position: { lat: position.coords.latitude, lng: position.coords.longitude },
+	map: map,
+	title: "Hello world!"
+});
 
-	});
-
-	marker = new google.maps.Marker({ 
 	
-		position:{lat: 42.983233, lng: -81.250688 },
-		map: map,
-		title: "Hello world!"
-
-	});
 }
 
-	initMap();
+if(navigator.geolocation){
+
+navigator.geolocation.getCurrentPosition(initMap, handleError); //this function requirs a success callback and an error callback. The error callback runs when it fails.
+}else{
+
+	console.log("Failure to load.");
+}
+
+function handleError() {
+
+console.log("Something went wrong.");
+
+}
+
 
 })();
