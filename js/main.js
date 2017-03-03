d469702f48486be4d483e5371077621e29e845a7
Clinton <c_clinton2@tss-hr420-dm38.fc.ca>
2017-03-03 16:20:57 -0500

    geolocation, map drawing features added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,105 @@
 (function() {
 
-var map, marker;
+var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker, preloader = document.querySelector('.preload-wrapper');
 
-function initMap(){
+///import the gocode API
+///
+var geocoder = new google.maps.Geocoder(),
+geocodeButton = document.querySelector('.geocode'),
 
-	map = new google.maps.Map(document.querySelector('.map-wrapper'), {
+//directions display
+directionsService = new google.maps.DirectionsService(),
+directionsDisplay,
+locations = [];
 
-		center: {lat: 42.983233, lng: -81.250688},
-		zoom : 14
+function initMap(position){
+	//save our location
+	locations[0] = { lat: position.coords.latitude, lng: position.coords.longitude };
 
-	});
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setMap(map);
+	map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude }); //passes dynamic variables through method NOTE: objects require commas, methods require semicolons
+	map.setZoom(14);
 
-	marker = new google.maps.Marker({ 
+	marker = new google.maps.Marker({
+	position: { lat: position.coords.latitude, lng: position.coords.longitude },
+	map: map,
+	title: "Hello world!"
+
+
+});
+
+preloader.classList.add('hide-preloader');
 	
-		position:{lat: 42.983233, lng: -81.250688 },
-		map: map,
-		title: "Hello world!"
+}
+
+if(navigator.geolocation){
+
+navigator.geolocation.getCurrentPosition(initMap, handleError); //this function requirs a success callback and an error callback. The error callback runs when it fails.
+}else{
+
+	console.log("Failure to load.");
+}
+
+function handleError() {
+
+console.log("Something went wrong.");
+
+}
+
+function codeAddress(){
+
+	var address = document.querySelector('.address').value;
+
+	geocoder.geocode({ 'address' : address}, function(results, status){
+
+		if(status == google.maps.GeocoderStatus.OK){
+
+			// push location into array
+			locations[1] = { lat: results[0].geometry.location.lat(),
+							lng: results[0].geometry.location.lng(),
+					};
+			map.setCenter(results[0].geometry.location);
+
+			if(marker){
+				marker.setMap(null);
+
+				marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location,
+					});
+
+				calcRoute(results[0].geometry.location);
+			}
+
+			else {
+				console.log('Geocode was not successful for the following reason:', status);
+			}
+		}
 
 	});
 }
 
-	initMap();
+function calcRoute(codedLoc){
+
+var request = {
+
+	origin: locations[0],
+	destination: locations[1],
+	travelMode: 'DRIVING'
+};
+
+directionsService.route(request, function(response, status) {
+
+if(status == 'OK') {
+
+	directionsDisplay.setDirections(response);
+}
+});
+
+}
+
+geocodeButton.addEventListener('click', codeAddress, false);
+
 
 })();
